f0263194db61b7c4cdfa201d36667171fa5a4fe8
vacabun <maguotong66@gmail.com>
2023-06-20 16:29:54 +0800

    init pypi package

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import os
+import json
+import importlib
+import setuptools
+
+PROJ_NAME = 'sr-rec'
+PACKAGE_NAME = 'sr_rec'
+
+PROJ_METADATA = '%s.json' % PROJ_NAME
+
+here = os.path.abspath(os.path.dirname(__file__))
+proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
+
+README = open(os.path.join(here, 'README.md'), encoding='utf-8').read()
+
+VERSION = importlib.machinery.SourceFileLoader("version", os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).load_module().__version__
+
+setuptools.setup(
+    name=proj_info['name'],
+    version=VERSION,
+    author=proj_info['author'],
+    url=proj_info['url'],
+    author_email=proj_info['author_email'],
+    description=proj_info['description'],
+    keywords=proj_info['keywords'],
+    long_description=README,
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    test_suite='tests',
+    platforms='any',
+    zip_safe=True,
+    include_package_data=True,
+    entry_points={'console_scripts': proj_info['console_scripts']},
+    install_requires=proj_info['install_requires']
+)
---
+++ src/sr_rec/version.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+script_name = 'sr-rec'
+__version__ = '0.4.0'
