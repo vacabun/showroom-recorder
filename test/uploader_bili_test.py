7e2ec82b1a178ebd009cf492150f44437e58b11e
vacabun <maguotong66@gmail.com>
2023-06-27 22:45:33 +0800

    update uploder bilibili test.

--- test/uploader_bili_test.py
+++ test/uploader_bili_test.py
@@ -1,5 +1,5 @@
-from showroom_recorder.processor.uploader import UploaderBili
-from showroom_recorder.utils.config import get_bili_cookie
+from ..src.showroom_recorder.processor.uploader import UploaderBili
+from ..src.showroom_recorder.utils.config import get_bili_cookie
 import argparse
 
 
